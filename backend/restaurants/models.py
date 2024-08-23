97f131e347378308ef1ed939650c13b2d1a56090
Sam Watford <watford21221@gmail.com>
2024-08-23 10:37:33 -0500

    update load data script to ingest data to db

--- backend/restaurants/models.py
+++ backend/restaurants/models.py
@@ -5,6 +5,8 @@ class Restaurant(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     rating = models.FloatField(default=0)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    image_url = models.URLField(max_length=500, null=True, blank=True)
     image = models.ImageField(upload_to='restaurant_images/', null=True, blank=True)
 
     def __str__(self):
--- backend/restaurants/serializers.py
+++ backend/restaurants/serializers.py
@@ -4,4 +4,9 @@ from .models import Restaurant
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
-        fields = ['id', 'name', 'latitude', 'longitude', 'rating', 'image']
+        fields = ['id', 'name', 'rating', 'latitude', 'longitude', 'address', 'image_url', 'image']
+
+    def validate(self, data):
+        if not data.get('image_url') and not data.get('image'):
+            raise serializers.ValidationError("Either 'image_url' or 'image' must be provided.")
+        return data
